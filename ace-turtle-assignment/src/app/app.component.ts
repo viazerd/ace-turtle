9cb2488e002d24f7ca139fde0dfc422dac3e3202
Sandeep Vyas <viazerd@users.noreply.github.com>
2020-03-31 16:26:25 +0530

    Minor Changes

--- ace-turtle-assignment/src/app/app.component.ts
+++ ace-turtle-assignment/src/app/app.component.ts
@@ -13,23 +13,23 @@ export class AppComponent {
   @ViewChild("innerbox",{static:true}) innerBox: DragRef;
   @ViewChild("innerbox",{static:true}) innerbox: ElementRef;
 
-  height = 20;
-  width = 20;
+  height = 20;// Height of Inner Box
+  width = 20;// width of Inner Box
   dragPosition:Point = {x: 0, y: 180};
 
   constructor(private _sanitizer: DomSanitizer) { }
 
- get updatePos(){
+ get updatePos(){ // Updating the Position Dynamically
     console.log(this.dragPosition)
     this.innerBox.setFreeDragPosition
     return this._sanitizer.bypassSecurityTrustStyle(`translateX(${this.dragPosition.x}) translateY(${this.dragPosition.y})`);
   }
 
-  getPos(){
+  getPos(){//Checking for Updated Position
     this.updatePos
   }
 
-  dragEnd(event) {
+  dragEnd(event) {//Updating coordinates after drand and drop is finished
     let cords = event.source.getFreeDragPosition()
     this.dragPosition.x = cords.x
     this.dragPosition.y = cords.y
